c8bb498ce1dc9736d713685624feb9b7879d73b5
Carlosmzh <a2024112447@estudiantes.upsa.edu.bo>
2026-05-28 22:22:35 -0400

    reverse render

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Run with:
     streamlit run app.py
 """
 import sys
-#sys.dont_write_bytecode = True eliminar en producción para permitir .pyc y mejorar rendimiento
+sys.dont_write_bytecode = True #eliminar en producción para permitir .pyc y mejorar rendimiento
 from pathlib import Path
 
 # ── Ensure project root is on path ─────────────────────────────────────────────
